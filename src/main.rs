754b39f7ee8760e4239d2921c7efcb31b1463e72
Saksham Tandon <54137431+saksham1236@users.noreply.github.com>
2025-03-12 01:08:14 +0000

    Check for test

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use test_rust::add;
+
+fn main() {
+    let a = 1;
+    let b = 2;
+    let result = add(a, b);
+    println!("{} + {} = {}", a, b, result);
+}
---
+++ tests/test_lib.rs
@@ -0,0 +1,6 @@
+use test_rust::add;
+
+#[test]
+fn test_add() {
+    assert_eq!(add(1, 2), 3);
+}
